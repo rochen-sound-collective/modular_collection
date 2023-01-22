rochen-sound-collective/modular_collection

commit e30904009d30f5e60b8167cf6e0813b3af245527

Added Keyboard Mode setting. If Set to White Keys only, all black keys are blocked and also omitted when calculating the chord index.

collection/patterns/src/processors.rs
@@ -2,7 +2,8 @@ use std::collections::{BTreeMap, BTreeSet, VecDeque};
 use nih_plug::midi::NoteEvent::{NoteOn, NoteOff};
 use nih_plug::prelude::*;
 use crate::active_note::ActiveNoteDefaultData;
-use crate::utils::{get_note_of_event, get_chord_data};
+
+use crate::utils::{get_note_of_event, get_chord_data, KeyboardMode, count_black_keys_from_C3, is_black_key, raw_note_apply_keyboard_mode};
 
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Default)]
@@ -58,13 +59,16 @@ impl ChordPatternProcessor {
         process_note_event(e)
     } */
 
-    fn apply_pattern_changes(&mut self, send_events: &mut Vec<NoteEvent>, timing: u32, wrap_threshold: u8, octave_range: u8) {
+    fn apply_pattern_changes(&mut self, send_events: &mut Vec<NoteEvent>, timing: u32,
+                             wrap_threshold: u8, octave_range: u8, keyboard_mode: KeyboardMode) {
         // released keys
         while let Some(note_event) = self.released_pattern_keys.pop_back() {
-            let raw_note = get_note_of_event(&note_event).unwrap();
-            if let Some(active_note) = self.held_pattern_keys.remove(&raw_note) {
-                if let Some(modulated_event) = active_note.note_off(note_event.timing()) {
-                    send_events.push(modulated_event);
+            if let Some(raw_note) = raw_note_apply_keyboard_mode(
+                get_note_of_event(&note_event).unwrap(), &keyboard_mode){
+                if let Some(active_note) = self.held_pattern_keys.remove(&raw_note) {
+                    if let Some(modulated_event) = active_note.note_off(note_event.timing()) {
+                        send_events.push(modulated_event);
+                    }
                 }
             }
         }
@@ -88,26 +92,29 @@ impl ChordPatternProcessor {
         }
 
         // pressed keys
-        while let Some(note_event) = self.pressed_pattern_keys.pop_back() {
-            let raw_note = get_note_of_event(&note_event).unwrap();
-            let chord_data = get_chord_data(&self.chord.iter().cloned().collect(), raw_note, wrap_threshold, octave_range);
+        while let Some(note_event) = self.released_pattern_keys.pop_back() {
+          if let Some(raw_note) = raw_note_apply_keyboard_mode(
+                get_note_of_event(&note_event).unwrap(), &keyboard_mode) {
+              let chord_data = get_chord_data(&self.chord.iter().cloned().collect(), raw_note, wrap_threshold, octave_range);
 
-            let active_note = PatternData {
-                chord_data: chord_data,
-                note_data: ActiveNoteDefaultData::from_note_event(&note_event),
-            };
+              let active_note = PatternData {
+                  chord_data: chord_data,
+                  note_data: ActiveNoteDefaultData::from_note_event(&note_event),
+              };
 
-            if let Some(modulated_event) = active_note.note_on(note_event.timing()) {
-                send_events.push(modulated_event);
-            }
-            self.held_pattern_keys.insert(raw_note, active_note);
+              if let Some(modulated_event) = active_note.note_on(note_event.timing()) {
+                  send_events.push(modulated_event);
+              }
+              self.held_pattern_keys.insert(raw_note, active_note);
+          }
         }
     }
 
     //----------------------------
 
-    pub fn end_cycle(&mut self, send_events: &mut Vec<NoteEvent>, timing: u32, wrap_threshold: u8, octave_range: u8) {
-        self.apply_pattern_changes(send_events, timing, wrap_threshold, octave_range);
+    pub fn end_cycle(&mut self, send_events: &mut Vec<NoteEvent>, timing: u32, wrap_threshold: u8,
+                     octave_range: u8, keyboard_mode: KeyboardMode) {
+        self.apply_pattern_changes(send_events, timing, wrap_threshold, octave_range, keyboard_mode);
     }
 
     //----------------------------
@@ -151,6 +158,7 @@ mod tests {
     use nih_plug::midi::NoteEvent;
     use nih_plug::midi::NoteEvent::{NoteOn, NoteOff};
     use crate::processors::{PatternData, ChordPatternProcessor};
+    use crate::utils::KeyboardMode;
 
     #[test]
     fn test_process_chord_event() {
@@ -241,7 +249,7 @@ mod tests {
         });
 
         let send_events = &mut vec![];
-        processor.end_cycle(send_events, 0, 3, 12);
+        processor.end_cycle(send_events, 0, 3, 12, KeyboardMode::AllKeys);
 
         assert_eq!(*send_events, [
             NoteOn {
@@ -269,7 +277,7 @@ mod tests {
         });
 
         let send_events = &mut vec![];
-        processor.end_cycle(send_events, 1, 3, 12);
+        processor.end_cycle(send_events, 1, 3, 12, KeyboardMode::AllKeys);
 
         assert_eq!(*send_events, [
             NoteOff {

collection/patterns/src/utils.rs
@@ -1,6 +1,15 @@
 use nih_plug::midi::NoteEvent;
 use crate::processors::PatternChordData;
 use crate::utils;
+use nih_plug::prelude::*;
+
+#[derive(Enum, PartialEq)]
+pub enum KeyboardMode {
+    AllKeys = 0,
+    IgnoreBlackKeys = 1,
+    //ShiftBlackKeysRight = 2, //Not implemented
+    //ShiftBlackKeysLeft = 3
+}
 
 pub fn note_to_chord_idx_octave(note: u8, wrap_threshold: u8) -> (u8, i8) {
     (
@@ -13,19 +22,19 @@ pub fn note_to_chord_idx_octave(note: u8, wrap_threshold: u8) -> (u8, i8) {
     )
 }
 
-fn count_black_keys(note: u8) -> u8 {
+pub fn count_black_keys(note: u8) -> u8 {
     // Calculate the number of octaves between the lowest and the highest note
     let octaves = ((note) / 12) as u8;
     // Return the number of black keys in the octaves
     return (octaves * 5 + [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5][(note.rem_euclid(12)) as usize]) as u8;
 }
 
-fn count_black_keys_from_C3(note: u8) -> i32 {
+pub fn count_black_keys_from_C3(note: u8) -> i32 {
   // there are 25 black keys from C0 (0) to C3 (60)
   count_black_keys(note) as i32 - 25
 }
 
-fn is_black_key(note: u8) -> bool {
+pub fn is_black_key(note: u8) -> bool {
     match note % 12 {
         1 => true,  // C# / Db
         3 => true,  // D# / Eb
@@ -33,6 +42,17 @@ fn is_black_key(note: u8) -> bool {
         8 => true,  // G# / Ab
         10 => true, // A# / Bb
         _ => false,
+    }
+}
+
+pub fn raw_note_apply_keyboard_mode(raw_note: u8, keyboard_mode: &KeyboardMode)-> Option<u8> {
+    match keyboard_mode {
+        KeyboardMode::IgnoreBlackKeys =>
+          match is_black_key(raw_note) {
+              True=> None,
+              False => Some((raw_note as i32 - count_black_keys_from_C3(raw_note)) as u8)
+          }
+        KeyboardMode::AllKeys =>  Some(raw_note),
     }
 }
 

collection/patterns/src/lib.rs
@@ -10,7 +10,7 @@ use std::collections::{HashMap, HashSet};
 use std::mem::swap;
 use std::sync::{Arc, Mutex};
 use nih_plug::midi::NoteEvent::{NoteOn, NoteOff};
-use crate::utils::{get_note_of_event, set_note_of_event, get_chord_data};
+use crate::utils::{get_note_of_event, set_note_of_event, get_chord_data, KeyboardMode};
 
 pub struct Patterns {
     params: Arc<PatternsParams>,
@@ -30,6 +30,9 @@ struct PatternsParams {
 
     #[id = "octave_range"]
     octave_range: IntParam,
+
+    #[id = "key_mode"]
+    key_mode: EnumParam<KeyboardMode>,
 }
 
 impl Default for PatternsParams {
@@ -43,6 +46,7 @@ impl Default for PatternsParams {
             ),
             auto_threshold: BoolParam::new("Auto Threshold", true),
             octave_range: IntParam::new("Octave Range", 12, IntRange::Linear { min: 1, max: 127 }),
+            key_mode: EnumParam::new("Keyboard Mode", KeyboardMode::AllKeys),
         }
     }
 }
@@ -141,7 +145,7 @@ impl Plugin for Patterns {
             while let Some(event) = next_event {
                 if event.timing() != sample_id {
                     let note_events = &mut vec![];
-                    self.processor.end_cycle(note_events, sample_id, self.get_threshold(), self.params.octave_range.value() as u8);
+                    self.processor.end_cycle(note_events, sample_id, self.get_threshold(), self.params.octave_range.value() as u8, self.params.key_mode.value());
 
                     for e in note_events {
                         context.send_event(*e);
@@ -173,7 +177,7 @@ impl Plugin for Patterns {
             // does not change after the last note.
 
             let note_events = &mut vec![];
-            self.processor.end_cycle(note_events, sample_id, self.get_threshold(), self.params.octave_range.value() as u8);
+            self.processor.end_cycle(note_events, sample_id, self.get_threshold(), self.params.octave_range.value() as u8, self.params.key_mode.value());
 
             for e in note_events {
                 context.send_event(*e);

commit 17f4b9095499d9b1104c7d7bc2f3b8f6e7655cad

Fix a bug in processors.rs in ChordPatternProcessor::apply_pattern_changes introduced in commit e30904009d30f5e60b8167cf6e0813b3af245527 Fix a bug in utils.rs in ChordPatternProcessor::raw_note_apply_keyboard_mode introduced in commit e30904009d30f5e60b8167cf6e0813b3af245527
Added error handling in various functions.

Tests for utils.rs
- Added test_raw_note_apply_keyboard_mode
- Fixed test_is_black_key

collection/patterns/src/processors.rs
@@ -63,8 +63,8 @@ impl ChordPatternProcessor {
                              wrap_threshold: u8, octave_range: u8, keyboard_mode: KeyboardMode) {
         // released keys
         while let Some(note_event) = self.released_pattern_keys.pop_back() {
-            if let Some(raw_note) = raw_note_apply_keyboard_mode(
-                get_note_of_event(&note_event).unwrap(), &keyboard_mode){
+            if let Some(raw_note) = get_note_of_event(&note_event)
+                                        .and_then(|note| raw_note_apply_keyboard_mode(note, &keyboard_mode)){
                 if let Some(active_note) = self.held_pattern_keys.remove(&raw_note) {
                     if let Some(modulated_event) = active_note.note_off(note_event.timing()) {
                         send_events.push(modulated_event);
@@ -92,20 +92,20 @@ impl ChordPatternProcessor {
         }
 
         // pressed keys
-        while let Some(note_event) = self.released_pattern_keys.pop_back() {
-          if let Some(raw_note) = raw_note_apply_keyboard_mode(
-                get_note_of_event(&note_event).unwrap(), &keyboard_mode) {
-              let chord_data = get_chord_data(&self.chord.iter().cloned().collect(), raw_note, wrap_threshold, octave_range);
+        while let Some(note_event) = self.pressed_pattern_keys.pop_back() {
+            if let Some(raw_note) = get_note_of_event(&note_event)
+                                        .and_then(|note| raw_note_apply_keyboard_mode(note, &keyboard_mode)){
+                let chord_data = get_chord_data(&self.chord.iter().cloned().collect(), raw_note, wrap_threshold, octave_range);
 
-              let active_note = PatternData {
+                let active_note = PatternData {
                   chord_data: chord_data,
                   note_data: ActiveNoteDefaultData::from_note_event(&note_event),
-              };
+                };
 
-              if let Some(modulated_event) = active_note.note_on(note_event.timing()) {
+                if let Some(modulated_event) = active_note.note_on(note_event.timing()) {
                   send_events.push(modulated_event);
-              }
-              self.held_pattern_keys.insert(raw_note, active_note);
+                }
+                self.held_pattern_keys.insert(raw_note, active_note);
           }
         }
     }

collection/patterns/src/utils.rs
@@ -49,8 +49,8 @@ pub fn raw_note_apply_keyboard_mode(raw_note: u8, keyboard_mode: &KeyboardMode)-
     match keyboard_mode {
         KeyboardMode::IgnoreBlackKeys =>
           match is_black_key(raw_note) {
-              True=> None,
-              False => Some((raw_note as i32 - count_black_keys_from_C3(raw_note)) as u8)
+              true=> None,
+              false => u8::try_from(raw_note as i32 - count_black_keys_from_C3(raw_note)).ok()
           }
         KeyboardMode::AllKeys =>  Some(raw_note),
     }
@@ -68,7 +68,7 @@ pub fn get_chord_data(chord_vec: &Vec<u8>, note_value: u8, wrap_threshold: u8, o
     };
 
     if let Some(note) = chord_vec.get(chord_idx as usize) {
-        chord_data.triggered_note = Some((*note as i8 + octave_range as i8 * octave) as u8);
+        chord_data.triggered_note = u8::try_from(*note as i8 + octave_range as i8 * octave).ok();
     };
 
     chord_data
@@ -151,20 +151,10 @@ pub fn get_voice_id_of_event(note_event: &NoteEvent) -> Option<i32> { // Check i
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::{get_channel_of_event, get_chord_data, note_to_chord_idx_octave, is_black_key,
-                       count_black_keys_from_C3};
+    use crate::utils::{get_channel_of_event, get_chord_data, note_to_chord_idx_octave, is_black_key, count_black_keys_from_C3, raw_note_apply_keyboard_mode, KeyboardMode};
     use nih_plug::midi::NoteEvent;
     use crate::processors::PatternChordData;
-    
-    #[test]
-    fn test_is_black_key() {
-        // Test the is_black_key function with some example MIDI note numbers.
-        let black_keys = [1, 3, 6, 8, 10];
-        for note in black_keys {
-            println!("{}: {}", note, is_black_key(note));
-        }
-    }
-    
+
     #[test]
     fn test_count_black_keys() {
         assert_eq!(count_black_keys_from_C3(50), -4);
@@ -215,6 +205,35 @@ mod tests {
         assert_eq!((note_index, octave), (2, 0));
         let (note_index, octave) = note_to_chord_idx_octave(63, 3);
         assert_eq!((note_index, octave), (0, 1));
+    }
+
+    #[test]
+    fn test_is_black_key(){
+        assert!(!is_black_key(60)); // C3 = white
+        assert!(is_black_key(61)); // C#3 = black
+        assert!(!is_black_key(62)); // D3 = white
+        assert!(is_black_key(63)); // D#3 = black
+        assert!(!is_black_key(64)); // E3 = white
+        assert!(!is_black_key(65)); // F3 = white
+        assert!(is_black_key(66)); // F#3 = black
+        assert!(!is_black_key(67)); // G3 = white
+        assert!(is_black_key(68)); // G#3 = black
+        assert!(!is_black_key(69)); // A3 = white
+        assert!(is_black_key(70)); // A#3 = black
+        assert!(!is_black_key(71)); // B3 = white
+        assert!(!is_black_key(72)); // C4 = white
+    }
+
+    #[test]
+    fn test_raw_note_apply_keyboard_mode(){
+        assert_eq!(raw_note_apply_keyboard_mode(60, &KeyboardMode::AllKeys), Some(60));
+        assert_eq!(raw_note_apply_keyboard_mode(60, &KeyboardMode::IgnoreBlackKeys), Some(60));
+        assert_eq!(raw_note_apply_keyboard_mode(61, &KeyboardMode::AllKeys), Some(61));
+        assert_eq!(raw_note_apply_keyboard_mode(61, &KeyboardMode::IgnoreBlackKeys), None);
+        assert_eq!(raw_note_apply_keyboard_mode(62, &KeyboardMode::AllKeys), Some(62));
+        assert_eq!(raw_note_apply_keyboard_mode(62, &KeyboardMode::IgnoreBlackKeys), Some(61));
+        assert_eq!(raw_note_apply_keyboard_mode(63, &KeyboardMode::AllKeys), Some(63));
+        assert_eq!(raw_note_apply_keyboard_mode(63, &KeyboardMode::IgnoreBlackKeys), None);
     }
 
     #[test]